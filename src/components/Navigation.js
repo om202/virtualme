f980351f6be8ec6c09eef6a9637b6d5ee32b3cbe
Omprakash Sah Kanu <opsworld.g@gmail.com>
2024-02-16 13:22:30 -0700

    fixed nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,6 @@ import { Navbar, Nav } from "react-bootstrap";
 import "./Navigation.css";
 import { AppConst } from "../AppConst";
 
-
 export const Navigation = () => {
   return (
     <Navbar expand="lg" className="shadow-sm navbar fixed-top">
@@ -11,7 +10,9 @@ export const Navigation = () => {
         <img src="./logo.png" alt="logo" className="brand-logo" />
         {AppConst.APP_NAME}
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggle"/>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggle">
+        <i class="bi bi-list toggle-icon"></i>
+      </Navbar.Toggle>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="/">Home</Nav.Link>
