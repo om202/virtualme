8c48dfbd15cece049d95ab9ba1d226d9627ab19b
Omprakash Sah Kanu <opsworld.g@gmail.com>
2024-02-16 12:54:24 -0700

    banner

---
+++ src/components/HomeBanner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeBanner.css";
+
+export default function HomeBanner() {
+  return (
+    <div className="home-banner">
+      <div className="home-banner__content">
+        <h1 className="home-banner__title">Realistic Chatbots</h1>
+        <p className="home-banner__description">
+          Virtual.me is a platfrom that allows you to chat with your favorite
+          characters.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import "./Home.css";
 import { BotData } from "../botdata";
 import { useNavigate } from "react-router-dom";
 import Navigation from "../components/Navigation";
+import HomeBanner from "../components/HomeBanner";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <>
       <Navigation />
+      <HomeBanner />
       <div className="container-fluid home-container">
         <div className="row title-button-row">
           <div className="title">Choose a Character</div>
